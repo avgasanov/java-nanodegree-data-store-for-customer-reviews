d7c8d62bc42a236642d430c4cdd0c286692eab99
AVGasanov <avgasanov@gmail.com>
2019-09-08 23:25:27 +0400

    Add test for ReviewRepo. Closes #1
    - remove obsolete imports

--- src/main/java/com/udacity/course3/reviews/repository/mongodb/ReviewRepo.java
+++ src/main/java/com/udacity/course3/reviews/repository/mongodb/ReviewRepo.java
@@ -3,7 +3,6 @@ package com.udacity.course3.reviews.repository.mongodb;
 import com.udacity.course3.reviews.model.mongodb.Review;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ReviewRepo extends MongoRepository<Review,Integer> {
--- src/test/java/com/udacity/course3/reviews/repository/mongodb/ReviewRepoTest.java
+++ src/test/java/com/udacity/course3/reviews/repository/mongodb/ReviewRepoTest.java
@@ -1,6 +1,5 @@
 package com.udacity.course3.reviews.repository.mongodb;
 
-import com.udacity.course3.reviews.model.mongodb.Product;
 import com.udacity.course3.reviews.model.mongodb.Review;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,24 +10,22 @@ import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
 
 @ExtendWith(SpringExtension.class)
 @DataMongoTest
 @RunWith(SpringRunner.class)
-public class ProductRepoTest {
+public class ReviewRepoTest {
 
     @Autowired
-    private ProductRepo productRepo;
+    private ReviewRepo reviewRepo;
 
     @Test
-    public void verify_findByIdAndName() {
-        String name = "some name";
+    public void verify_findByIdAndUsername() {
         Integer id = 0;
-        Product testProduct = new Product(id, name, new ArrayList<Review>());
-        productRepo.save(testProduct);
-        Product repoProduct = productRepo.findByIdAndName(id, name).orElse(null);
-        Assert.assertEquals(testProduct, repoProduct);
+        String username = "some name";
+        Review reviewTest = new Review(id, username, null, null);
+        reviewRepo.save(reviewTest);
+        Review repoReview = reviewRepo.findByIdAndUsername(id, username).orElse(null);
+        Assert.assertEquals(reviewTest, repoReview);
     }
-
 }
